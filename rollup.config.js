d2d7ce4d5ebcec757da4694f5efa36992fc9cadd
Mike Clark <mike.clark@nvoq.com>
2024-05-08 10:26:58 -0600

    use dts-buddy to generate bundled .d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,76 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import shelljs from 'shelljs';
+import terser from '@rollup/plugin-terser';
+import { createBundle as dtsBuddyCreateBundle } from 'dts-buddy';
+import { createRequire } from "module";
+const Require = createRequire(import.meta.url);
+const packageJson = Require("./package.json");
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const config = {
-    input: 'src/edit-distance.ts',
-    output: [
-        { file: 'dist/bundle.cjs.js', format: 'cjs' },
-        { file: 'dist/bundle.umd.js', format: 'umd', name: 'edit_distance' },
-        { file: 'dist/bundle.amd.js', format: 'amd', name: 'edit_distance' },
-        { file: 'dist/bundle.iife.js', format: 'iife', name: 'edit_distance' },
-        { file: 'dist/bundle.esm.js', format: 'es' }
-    ],
-    plugins: [
-        nodeResolve(), // resolves node modules
-        commonjs(), // converts commonjs to ES modules
-        typescript(),
-    ]
-};
+function buildConfig() {
 
-if (isProduction) {
-    config.plugins.push(terser());
+    const config = {
+        input: 'src/edit-distance.ts',
+        output: [
+            { file: packageJson.main, format: 'cjs' },
+            { file: packageJson.module, format: 'es' },
+            { file: packageJson.unpkg, format: 'umd', name: 'edit_distance' },
+            { file: packageJson.browser, format: 'iife', name: 'edit_distance' },
+            { file: packageJson.types, format: 'es' }
+        ],
+        plugins: [
+            prebuildClean(),
+            nodeResolve(),
+            commonjs(),
+            typescript(),
+            isProduction && terser(),
+            dts(),
+            postbuildClean(),
+        ]
+    };
+
+    function prebuildClean() {
+        return {
+            name: 'cleanup',
+            order: 'pre',
+            buildStart() {
+                shelljs.rm('-rf', 'dist');
+            }
+        };
+    }
+
+    function postbuildClean() {
+        return {
+            name: 'cleanup',
+            order: 'post',
+            closeBundle() {
+                shelljs.rm('-rf', 'dist/types');
+            }
+        };
+    }
+
+    function dts() {
+        return {
+            name: 'dts',
+            closeBundle() {
+                console.log('Generating dts bundle...');
+                dtsBuddyCreateBundle({
+                    project: 'tsconfig.json',
+                    output: packageJson.types,
+                    modules: {
+                        'edit-distance': 'src/edit-distance.ts',
+                    },
+                });
+            }
+        };
+    }
+
+    return config;
 }
 
-export default config;
+
+
+export default buildConfig();
